f0f9a302469bb20ad7dbd7d2bddd81996e8ae98b
Steven Yung <stevenyung@icloud.com>
2020-02-20 21:10:14 +0100

    update index

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-module.exports = {
-  plugins: [
-    [
-      "@vuepress/blog",
+const pick = require("lodash/pick");
+
+module.exports = themeConfig => {
+  themeConfig = {
+    name: "Steven Yung",
+    description: "A developer trying to break out of traditional 9-5.",
+    profile: "/profile.jpg",
+    socialLinks: [
+      { url: "https://twitter.com/stvnyung", name: "Twitter" },
+      { url: "https://github.com/stvnyung", name: "Github" },
+      { url: "https://dev.to/stvnyung", name: "Dev.to" },
+      { url: "https://www.instagram.com/stvn.yung/", name: "Instagram" }
+    ]
+  };
+
+  const defaultBlogPluginOptions = {
+    directories: [
       {
-        directories: [
-          {
-            id: "post",
-            dirname: "_posts",
-            path: "/",
-            itemPermalink: "/:slug"
-          }
-        ]
+        id: "post",
+        dirname: "_posts",
+        path: "/",
+        itemPermalink: "/:slug"
       }
-    ],
+    ]
+  };
+
+  const properties = [
+    "directories",
+    "frontmatters",
+    "globalPagination",
+    "sitemap",
+    "comment",
+    "newsletter"
+  ];
+  const themeConfigPluginOptions = {
+    ...pick(themeConfig, properties)
+  };
+
+  const blogPluginOptions = Object.assign(
+    {},
+    defaultBlogPluginOptions,
+    themeConfigPluginOptions
+  );
+
+  const plugins = [
+    ["@vuepress/blog", blogPluginOptions],
     [
       "@vuepress/google-analytics",
       {
@@ -28,5 +59,43 @@ module.exports = {
           "https://icloud.us20.list-manage.com/subscribe/post?u=b334cab704e8cf9d86349a6c1&amp;id=27383beab6"
       }
     ]
-  ]
+  ];
+
+  const config = {
+    plugins
+  };
+
+  return config;
 };
+// module.exports = {
+//   plugins: [
+//     [
+//       "@vuepress/blog",
+//       {
+//         directories: [
+//           {
+//             id: "post",
+//             dirname: "_posts",
+//             path: "/",
+//             itemPermalink: "/:slug"
+//           }
+//         ]
+//       }
+//     ],
+//     [
+//       "@vuepress/google-analytics",
+//       {
+//         ga: "UA-135207098-1"
+//       }
+//     ],
+//     [
+//       "vuepress-plugin-mailchimp",
+//       {
+//         // You need to provide this plugin with your Mailchimp endpoint in order for it
+//         // to know where to save the email address. See more detail in Config section.
+//         endpoint:
+//           "https://icloud.us20.list-manage.com/subscribe/post?u=b334cab704e8cf9d86349a6c1&amp;id=27383beab6"
+//       }
+//     ]
+//   ]
+// };
