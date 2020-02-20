f471146b415c89e1275056f9e49a3abc09a555c7
Steven Yung <stevenyung@icloud.com>
2020-02-20 23:24:46 +0100

    implement themeConfig on example

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  title: "Steven Yung",
-  theme: require.resolve("../../"),
-  themeConfig: {},
   postcss: {
     plugins: [require("tailwindcss")("./tailwind.js"), require("autoprefixer")]
   }
+  title: "Steven Yung",
+  theme: require.resolve("../../"),
+  themeConfig: {
+    name: "Steven Yung",
+    description: "A developer trying to break out of traditional 9-5.",
+    profile: "/profile.jpg",
+    socialLinks: [
+      { url: "https://twitter.com/stvnyung", name: "Twitter" },
+      { url: "https://github.com/stvnyung", name: "Github" },
+      { url: "https://dev.to/stvnyung", name: "Dev.to" },
+      { url: "https://www.instagram.com/stvn.yung/", name: "Instagram" }
+    ]
+  },
 };
