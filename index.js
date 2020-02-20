24b508b6c7bf77d1c8a77dd0374cc0dec59b4cab
Steven Yung <stevenyung@icloud.com>
2020-02-20 21:22:16 +0100

    WIP: disable google analytics and mailchimp

--- index.js
+++ index.js
@@ -43,22 +43,22 @@ module.exports = themeConfig => {
   );
 
   const plugins = [
-    ["@vuepress/blog", blogPluginOptions],
-    [
-      "@vuepress/google-analytics",
-      {
-        ga: "UA-135207098-1"
-      }
-    ],
-    [
-      "vuepress-plugin-mailchimp",
-      {
-        // You need to provide this plugin with your Mailchimp endpoint in order for it
-        // to know where to save the email address. See more detail in Config section.
-        endpoint:
-          "https://icloud.us20.list-manage.com/subscribe/post?u=b334cab704e8cf9d86349a6c1&amp;id=27383beab6"
-      }
-    ]
+    ["@vuepress/blog", blogPluginOptions]
+    // [
+    //   "@vuepress/google-analytics",
+    //   {
+    //     ga: "UA-135207098-1"
+    //   }
+    // ],
+    // [
+    //   "vuepress-plugin-mailchimp",
+    //   {
+    //     // You need to provide this plugin with your Mailchimp endpoint in order for it
+    //     // to know where to save the email address. See more detail in Config section.
+    //     endpoint:
+    //       "https://icloud.us20.list-manage.com/subscribe/post?u=b334cab704e8cf9d86349a6c1&amp;id=27383beab6"
+    //   }
+    // ]
   ];
 
   const config = {
